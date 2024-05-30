7e7f825ecfc21bb195841a16a5c1e61589df6990
rahulchodary7 <rahulanubolu@gmail.com>
2024-05-30 08:46:53 +0530

    helo

---
+++ src/main/java/com/example/RepeatName.java
@@ -0,0 +1,27 @@
+package com.exmaple;
+import java .util.*;
+/**
+ * RepeatName
+ */
+public class RepeatName {
+
+    public static void main(String[] args) {
+       Scanner scanner = new scanner (System.in);
+       int n = scanner.nextint();
+       String name = scanner.nextLine();
+       for(int i=0; i<n; i++);{
+        System.out.println("rahul" + i);
+       } 
+int count =0;
+while (count<n); {
+    System.out.println("rahul"+ count);
+    count ++; 
+    
+}
+ int docunt =0;
+ do{
+    System.out.println("rahul" + docunt);
+ }
+ while(docount<n) ;     
+    }
+}
